9c017543437f4ac72cceaa664c67b837ac4c7fb0
TheMuffinlord <tcgiant@gmail.com>
2025-07-10 16:59:55 -0400

    ch3.1

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+
+system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+
 if len(sys.argv) == 1:
     sys.exit("prompt must be provided")
 else:
@@ -15,7 +18,11 @@ else:
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        )
     print(response.text)
     if "--verbose" in sys.argv:
         print(f"User prompt:", user_prompt)
