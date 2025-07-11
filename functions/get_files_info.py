63a026db56303f073a6bd5c624c9994578e57523
TheMuffinlord <tcgiant@gmail.com>
2025-07-11 15:06:11 -0400

    ch3.2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,11 +1,16 @@
 import os
 
+from google import genai
+from google.genai import types
+
 def get_files_info(working_directory, directory=None):
     fullpath = os.path.join(working_directory, directory)
     wd_paths = os.listdir(working_directory)
-    if os.path.isfile(fullpath):
+    abs_fullpath = os.path.abspath(fullpath)
+    abs_workdir = os.path.abspath(working_directory)
+    if os.path.isfile(abs_fullpath):
         raise Exception(f'Error: "{directory}" is not a directory')
-    elif directory not in wd_paths and directory != ".":
+    if not abs_fullpath.startswith(abs_workdir):
         raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
     pathlist = os.listdir(fullpath)
     if directory == None or directory == ".":
@@ -19,8 +24,17 @@ def get_files_info(working_directory, directory=None):
         result_string += f"- {path}: file_size={ofp_size} bytes, is_dir={ofp_isdir}\n"
     return result_string
 
-        
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)        
 
-    
-
-#i will write this tomorrow. fuck you
